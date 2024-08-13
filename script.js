03b1264f7f4a18ca4145ddc61282fade98bfb486
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-08-13 14:23:41 -0700

    Add basic toggle to check box, sized list items/list item's content

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("RUNING Running")
+console.log("RUNING Running");
+
+const checkbox = document.getElementsByClassName("checkbox");
+
+addCheckboxEventListeners();
+
+function addCheckboxEventListeners() {
+    for (let i = 0; i < checkbox.length; i++) {
+        console.log(checkbox[i]);
+        checkbox[i].addEventListener("click", toggleCheckbox);
+    }
+}
+
+function toggleCheckbox() {
+    this.classList.toggle("checked");
+}
